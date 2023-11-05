0e8f644f14a9c0ce0adc8c4cd95b5c7bce895b0f
kolasniwash <batchbrewdata@gmail.com>
2023-11-05 19:26:21 +0100

    remove unused typeform models

--- app/model.py
+++ app/model.py
@@ -1,48 +1,6 @@
 from typing import List, Optional
 from pydantic import BaseModel
 
-class Choice(BaseModel):
-    id: str
-    label: str
-    ref: str
-
-class Field(BaseModel):
-    id: str
-    type: str
-    ref: str
-
-class Ending(BaseModel):
-    id: str
-    ref: str
-
-class Answer(BaseModel):
-    type: str
-    choice: Optional[Choice] = None
-    date: Optional[str] = None
-    text: Optional[str] = None
-    field: Field
-
-class FormDefinition(BaseModel):
-    id: str
-    title: str
-    fields: List[Field]
-    endings: List[Ending]
-
-class FormResponse(BaseModel):
-    form_id: str
-    token: str
-    landed_at: str
-    submitted_at: str
-    definition: FormDefinition
-    answers: List[Answer]
-    ending: Ending
-
-class TypeFormResponse(BaseModel):
-    event_id: str
-    event_type: str
-    form_response: FormResponse
-
-
 class GameInput(BaseModel):
     file: str
     our_score: int
